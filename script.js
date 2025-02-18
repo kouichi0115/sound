6a48daaa19205258fa40ae433ee940a323343286
kouichi0115 <kouichi.eagles@gmail.com>
2025-02-19 08:59:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+let audio = new Audio(); // 1つのオーディオインスタンスを作る
+
 document.querySelectorAll('.soundButton').forEach(button => {
     button.addEventListener('click', function() {
-        let audio = document.getElementById('audioPlayer');
+        if (!audio.paused) {
+            audio.pause();  // 再生中なら一旦停止
+            audio.currentTime = 0; // 音の再生位置を最初に戻す
+        }
         audio.src = this.dataset.sound;
         audio.play();
     });
@@ -8,7 +13,8 @@ document.querySelectorAll('.soundButton').forEach(button => {
 
 // 音を止めるボタンの処理
 document.getElementById('stopButton').addEventListener('click', function() {
-    let audio = document.getElementById('audioPlayer');
-    audio.pause();  // 再生を一時停止
-    audio.currentTime = 0; // 音の再生位置を最初に戻す
+    if (!audio.paused) {
+        audio.pause();  // 音を停止
+        audio.currentTime = 0; // 再生位置をリセット
+    }
 });
